4c3bb8e1cb8a3058040a0795a35881615e461cfb
LiutaSvitlana <liuta.dev@gmail.com>
2025-07-04 20:44:40 +0300

    initial commit

---
+++ JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Svitlana Liuta on 02.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
