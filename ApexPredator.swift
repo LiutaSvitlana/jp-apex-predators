a721bd620253b106ef198df27f28b0e3eb0efbb5
LiutaSvitlana <liuta.dev@gmail.com>
2025-07-07 19:15:54 +0300

    implement model for decoding ApexPredator JSON data

---
+++ ApexPredator.swift
@@ -0,0 +1,27 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Svitlana Liuta on 07.07.2025.
+//
+
+import SwiftUI
+
+struct ApexPredator: Decodable {
+    
+    let id: Int
+    let name: String
+    let type: String
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    struct MovieScene: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+    
+}
---
+++ Preview Content/ApexPredator.swift
@@ -0,0 +1,7 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Svitlana Liuta on 07.07.2025.
+//
+
