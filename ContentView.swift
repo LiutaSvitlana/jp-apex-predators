c53c5b4c5011236acf3cdf89227a9e9123aa0478
LiutaSvitlana <liuta.dev@gmail.com>
2025-07-10 22:21:33 +0300

    add search functionality with real-time filtering

--- ContentView.swift
+++ ContentView.swift
@@ -10,31 +10,56 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators // вся колекция
+        } else {
+            return predators.apexPredators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText) // если юзер введет текст разными регистрами
+            }
+        }
+    }
+    
     var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                // Dinosaur image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading) {
-                    // Name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    // Type
-                    Text(predator.type.rawValue.capitalized) //добавляем rawValue
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        // Dinosaur image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack(alignment: .leading) {
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
                 }
             }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText) // анимация при поиске
         }
         .preferredColorScheme(.dark)
     }
